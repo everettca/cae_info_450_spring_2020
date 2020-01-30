9988d625d98b0b8a70e17a4c055ce5116ef50b3f
everettca <60232335+everettca@users.noreply.github.com>
2020-01-30 12:31:00 -0500

    Add files via upload

---
+++ week2/practice2.cpp
@@ -0,0 +1,45 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberinput = 1, i;
+
+    while (numberinput > 0)
+    {
+        int c = 0;
+        cout << "Enter your Number: ";
+        cin >> numberinput;
+        if (numberinput > 0)
+        {
+
+            for (i = 1; i <= numberinput; i++)
+            {
+                if (numberinput % i == 0)
+                {
+                    c++;
+                }
+            }
+
+            if (c == 2)
+            {
+                cout << numberinput << " is a prime number." << endl;
+            }
+            else
+            {
+                cout << numberinput << " is not a prime number." << endl;
+            }
+        }
+
+        else
+        {
+            break;
+        }
+
+        continue;
+    }
+
+    return 0;
+}
